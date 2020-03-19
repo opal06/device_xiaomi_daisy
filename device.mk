33aa48c38cc125dc3afcf5fc5dee6f0c9fd53f6e
acras01 <acras1@gmail.com>
2020-03-19 13:54:32 +0100

    Daisy: Update device.mk with new camera services

--- device.mk
+++ device.mk
@@ -163,8 +163,13 @@ PRODUCT_COPY_FILES += \
 
 # Camera
 PRODUCT_PACKAGES += \
+    android.frameworks.cameraservice.common@2.0 \
+    android.frameworks.cameraservice.device@2.0 \
+    android.frameworks.cameraservice.service@2.0 \
+    android.frameworks.displayservice@1.0_32 \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service \
+    android.hardware.camera.provider@2.5 \
     Snap
 
 # Consumerir
