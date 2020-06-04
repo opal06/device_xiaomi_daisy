cde2e25875fc0185dddc5dad411428ab48bf81e2
lyceris-chan <ghostdrain@outlook.com>
2020-06-04 19:03:43 +0200

    Daisy: Edit some render related props

--- prop.mk
+++ prop.mk
@@ -87,6 +87,8 @@ persist.camera.video.CDS=off \
 persist.camera.eis.enable=1 \
 persist.camera.dual.camera=0 \
 persist.camera.gyro.disable=0 \
+persist.camera.HAL3.enabled=1 \
+persist.vendor.camera.HAL3.enabled=1 \
 persist.camera.isp.clock.optmz=0 \
 persist.camera.stats.test=5 \
 persist.vendor.qti.telephony.vt_cam_interface=1 \
@@ -127,26 +129,15 @@ persist.dirac.poolsize=3
 
 # Display
 PRODUCT_PROPERTY_OVERRIDES += \
-debug.egl.hw=0 \
-debug.enable.sglscale=1 \
-debug.gralloc.enable_fb_ubwc=1 \
-debug.mdpcomp.logs=0 \
+debug.hwui.use_buffer_age=false \
 debug.sf.enable_hwc_vds=1 \
-debug.sf.hw=0 \
-debug.sf.latch_unsignaled=0 \
-debug.sf.recomputecrop=0 \
-dev.pm.dyn_samplingrate=1 \
-persist.demo.hdmirotationlock=false \
-persist.hwc.enable_vds=1 \
-persist.hwc.mdpcomp.enable=true \
+debug.sf.hw=1 \
 ro.opengles.version=196610 \
-ro.qualcomm.cabl=0 \
-ro.sf.lcd_density=420 \
-debug.sdm.support_writeback=0 \
-ro.vendor.display.cabl=2 \
-sdm.debug.disable_skip_validate=1 \
-vendor.display.enable_default_color_mode=1 \
+vendor.display.disable_partial_split=1 \
+vendor.display.disable_rotator_downscale=1 \
 vendor.display.disable_skip_validate=1 \
+vendor.display.enable_default_color_mode=0 \
+vendor.display.perf_hint_window=50 \
 vendor.gralloc.enable_fb_ubwc=1 \
 persist.vendor.max.brightness=475
 
@@ -170,20 +161,6 @@ vendor.hw.fm.init=0
 # Frp
 PRODUCT_PROPERTY_OVERRIDES += \
 ro.frp.pst=/dev/block/bootdevice/by-name/config
-
-# HWUI
-PRODUCT_PROPERTY_OVERRIDES += \
-ro.hwui.texture_cache_size=72 \
-ro.hwui.layer_cache_size=48 \
-ro.hwui.r_buffer_cache_size=8 \
-ro.hwui.path_cache_size=32 \
-ro.hwui.gradient_cache_size=1 \
-ro.hwui.drop_shadow_cache_size=6 \
-ro.hwui.texture_cache_flushrate=0.4 \
-ro.hwui.text_small_cache_width=1024 \
-ro.hwui.text_small_cache_height=1024 \
-ro.hwui.text_large_cache_width=2048 \
-ro.hwui.text_large_cache_height=1024
 
 # LMKD
 PRODUCT_PROPERTY_OVERRIDES += \
@@ -299,12 +276,30 @@ persist.sys.fflag.override.settings_network_and_internet_v2=true
 
 # SurfaceFlinger
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
+ro.surface_flinger.protected_contents=true \
 ro.surface_flinger.force_hwc_copy_for_virtual_displays=true \
 ro.surface_flinger.max_virtual_display_dimension=4096 \
-ro.surface_flinger.protected_contents=true \
+ro.surface_flinger.max_virtual_display_dimension=4096 \
 ro.surface_flinger.vsync_event_phase_offset_ns=2000000 \
 ro.surface_flinger.vsync_sf_event_phase_offset_ns=6000000 \
-ro.surface_flinger.use_color_management=true
+ro.surface_flinger.max_frame_buffer_acquired_buffers=3
+
+PRODUCT_PROPERTY_OVERRIDES += \
+debug.sf.early_phase_offset_ns=1500000 \
+debug.sf.early_app_phase_offset_ns=1500000 \
+debug.sf.early_gl_phase_offset_ns=3000000 \
+debug.sf.early_gl_app_phase_offset_ns=15000000 \
+debug.sf.latch_unsignaled=1 \
+debug.sf.disable_backpressure=1 \
+debug.sf.enable_gl_backpressure=1
+
+# The default sf phase offset is set to 6ms, to avoid it be included into next
+# vsync threshold, set high fps early sf and next vsync threshold phase offset
+# to 6.1ms, which is bigger than all sf phase offsets in normal frame rate.
+PRODUCT_PROPERTY_OVERRIDES += \
+debug.sf.high_fps_early_phase_offset_ns=6100000 \
+debug.sf.high_fps_early_gl_phase_offset_ns=9000000 \
+debug.sf.phase_offset_threshold_for_next_vsync_ns=6100000
 
 # Thermal configs path
 PRODUCT_PROPERTY_OVERRIDES += \
